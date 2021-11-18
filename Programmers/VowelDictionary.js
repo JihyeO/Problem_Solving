eadb986f83d2ddbbf371134d5a2b744dc15015f2
ojihye <jihyegnb@naver.com>
2021-11-18 22:15:43 +0900

    programmers weekly challenge #3

---
+++ Programmers/VowelDictionary.js
@@ -0,0 +1,27 @@
+/*
+ * 모음 사전
+ * 빈 문자에서 시작하여 5글자 이내의 모든 경우의 수를 구하면서 '단어(key)와 1씩 더한 값(value)'의 쌍으로 저장, 구하고자 하는 단어의 값을 반환한다.
+ */
+
+let order = 0;
+let dictionary = {};
+
+function solution(target) {
+    makeDictionary("", target);
+    return dictionary[target];
+}
+
+function makeDictionary(word) {
+    dictionary[word] = order;
+    order += 1;
+
+    const alphabet = ["A", "E", "I", "O", "U"];
+    for(let i = 0; i < 5; i ++) {
+        if (word.length < 5) {
+            makeDictionary(word+alphabet[i]);
+        } else {
+            break;
+        }
+    }
+    return;
+}
